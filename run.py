6a47006a72a820e52d5dbb8ccdcdbb19c9b32154
Sander Vocke <s149737@mammoth.(none)>
2015-05-14 14:26:19 +0200

    made run.py work on linux.

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 #FUNCTIONS FOR RUNNING SIMULATIONS
-
+import os
 import config
 from subprocess import Popen, call
 	
 #runs the simulation based on the generated XML in the ./sim folder.
 def runSim():
-	call(['./sim/rotalumis.exe', '-f', 'dse_gen.xml'], cwd = './sim')
+	if os.name == 'nt':
+		call(['./sim/rotalumis.exe', '-f', 'dse_gen.xml'], cwd = './sim')
+	elif os.name == 'posix':
+		os.chdir('./sim')
+		call(['./rotalumis', '-f', 'dse_gen.xml'])
+		os.chdir('./../')
